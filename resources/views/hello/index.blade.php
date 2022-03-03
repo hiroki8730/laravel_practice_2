fc66b0bba423bb4ddbce2a5433986db2226140f6
hiroki8730 <funassynassyyy@ezweb.ne.jp>
2022-03-03 23:57:38 +0900

    bladeについて学んだ

---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>Hello/Index</title>
+    <style>
+        body {font-size:16pt; color:#999; }
+        h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px}
+    </style>
+</head>
+<body>
+    <h1>Blade/Index</h1>
+    <p>&#064;foreachディレクティブの例</p>
+    @foreach($data as $item)
+    @if($loop->first)
+    <p>※データ一覧</p>
+    @endif
+    <li>No,{{$loop->iteration}}. {{$item}}</li>
+    @if($loop->last)
+    </ul><p>--ここまで</p>
+    @endif
+    @endforeach
+</body>
