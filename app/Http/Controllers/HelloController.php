8f0dc73eb23151b1d055e0753a8d34812c3a4dd0
hiroki8730 <funassynassyyy@ezweb.ne.jp>
2022-03-07 23:50:07 +0900

    データベースの利用について学んだ

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
@@ -9,23 +10,64 @@ use Validator;
 
 class HelloController extends Controller
 {
-    public function index (Request $request) {
-        if ($request->hasCookie('msg')) {
-            $msg = 'Cookie: ' . $request->cookie('msg');
-        } else {
-            $msg = '※クッキーはありません。';
-        }
-        return view('hello.index', ['msg'=> $msg ]);
+    public function index (Request $request) 
+    {
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items'=>$items]);
     }
 
-    public function post (Request $request) {
-        $validate_rule = [
-            'msg' => 'required',
+    public function post (Request $request) 
+    {
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items'=>$items]);
+    }
+
+    public function add (Request $request)
+    {
+        return view('hello.add');
+    }
+
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
         ];
-        $this->validate($request, $validate_rule);
-        $msg = $request->msg;
-        $response = new Response(view('hello.index', ['msg' => '「' . $msg . '」をクッキーに保存しました。']));
-        $response->cookie('msg', $msg, 100);
-        return $response;
+        DB::insert('insert into people (name, mail, age) values(:name, :mail, :age)', $param);
+        return redirect('/hello');
+    }
+
+    public function edit (Request $request)
+    {
+        $param = ['id' => $request->id];
+        $item = DB::select('select * from people where id = :id', $param);
+        return view('hello.edit', ['form' => $item[0]]);
+    }
+
+    public function update (Request $request) 
+    {
+        $param = [
+            'id' => $request->id,
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::update('update people set name =:name, mail = :mail, age = :age where id = :id', $param);
+        return redirect('/hello');
+    }
+
+    public function del (Request $request)
+    {
+        $param = ['id' => $request->id];
+        $item = DB::select('select * from people where id = :id', $param);
+        return view('hello.del', ['form' => $item[0]]);
+    }
+
+    public function remove (Request $request)
+    {
+        $param = ['id' => $request->id];
+        DB::delete('delete from people where id = :id', $param);
+        return redirect('/hello');
     }
 }
